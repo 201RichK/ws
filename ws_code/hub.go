9a4877ea074765dedc2e3a09da0477f31c48a694
201RichK <amosamospascal@yahoo.com>
2020-03-28 22:24:54 +0000

    adding room

--- ws_code/hub.go
+++ ws_code/hub.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
 )
 
-//Hub coontrol a bunch of room
-type Hub struct {
+//Hub maintains the set of active client and broadcasr msg to the client
+type hub struct {
+	//hub for control room
 	hub map[string]*room
+
+	//Registered clients
+	clients map[*Client]bool
+
+	//Inbound messages from the client
+	broadcast chan message
+
+	//register request from the client
+	register chan *Client
+
+	//Unregister request from the client
+	unregister chan *Client
 }
 
 var (
@@ -19,19 +30,41 @@ var (
 	}
 )
 
-//if room exist return it else create it
-func (h *Hub) getroom(name string) *room {
-	if _, ok := h.hub[name]; !ok {
-		h.hub[name] = newRoom(room)
+//NewHub register client by adding the client pointer
+//as a key in the client map
+func newHub() *hub {
+	return &hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan message),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
 	}
-	return h.hub[name]
 }
 
-func (h *Hub) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Error("upgrade error ::", err)
+//Run methode
+func (h *hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			//joining
+			log.Info(client)
+			h.clients[client] = true
+		case client := <-h.unregister:
+			//leaving
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			//send msg to all client
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
 	}
-
-	defer c.Close()
 }
